4587b50c95506513c9c1134b42576b666aa7cbdd
Victor Cortez <ctovictor@gmail.com>
2017-05-27 05:31:16 -0300

    adicionado exemplo de como criar uma nova função com sua propia thread dinamicamente

--- rascunhos.py
+++ rascunhos.py
@@ -1,14 +1,19 @@
 from tkinter import *
+import threading
 
 def sendMessage():
     print("hi there")
+    threading.Thread(name='window', target=window()).start()
 
-mainFrame = Tk()
-button = Button(mainFrame,text="send message",command=sendMessage)
-# button.place(relx=0.5,rely=0.5,anchor=CENTER)
-button.pack()
-# c = Canvas(mainFrame, width=720,height=400)
-# c.pack()
-# c.create_line(72,4,700,300)
+def window():
+    mainFrame = Tk()
+    button = Button(mainFrame,text="send message",command=sendMessage)
+    # button.place(relx=0.5,rely=0.5,anchor=CENTER)
+    button.pack()
+    # c = Canvas(mainFrame, width=720,height=400)
+    # c.pack()
+    # c.create_line(72,4,700,300)
 
-mainloop()
+    mainloop()
+
+window()
